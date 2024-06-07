5ebb82ce26a0a43fa24e45d5d3c89f4621826e2f
it-vojtechvasicek <it-vojtechvasicek@stredniskola.net>
2024-06-07 08:31:07 +0200

    Inicializoval jsem generátor náhodných čísel

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "iostream"
+#include "color.h"
+#include "ctime"
 using namespace std;
+
 /*Hadani tejneho cisla*/
 int main(int argc, char *argv[]){
+    srand(time(0));
     int tajneCislo;
     int hracovoCislo;
     tajneCislo = rand()%5+1; //náhodné cislo od 1 do 5
