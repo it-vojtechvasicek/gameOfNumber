a42f3ff5a544d019a788f22d21d44324590aba1e
it-vojtechvasicek <it-vojtechvasicek@stredniskola.net>
2024-06-07 09:02:17 +0200

    Inicializoval jsem generátor náhodných čísel

--- main.cpp
+++ main.cpp
@@ -9,7 +9,16 @@ int main(int argc, char *argv[]){
     int tajneCislo;
     int hracovoCislo;
     tajneCislo = rand()%5+1; //náhodné cislo od 1 do 5
-    cout << "Zadej svij typ na tajne čislo:";
+    cout << "Zadej svij typ na tajne cislo:";
     cin >> hracovoCislo;
+    if (tajneCislo == tajneCislo)
+    {
+        cout << "Uhodli jste tajne cislo!" << endl;
+    }
+    else
+    {
+        cout << "Neuhodli jste tajne cislo!" << endl;
+    }
+    
     return 0;
 }
